90db407766eae1f1d01e7254a1213ed3b26d453c
unknown <binh.le263@gmail.com>
2022-06-20 22:56:30 +0900

    init hw3,4

---
+++ class3_hw/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub struct School {
+    // !TODO
+}
+
+impl School {
+    pub fn new() -> School {
+        unimplemented!()
+
+    }
+
+    pub fn add(&mut self, grade: u32, student: &str) {
+        unimplemented!()
+
+        
+    }
+
+    pub fn grades(&self) -> Vec<u32> {
+        unimplemented!()
+
+    }
+
+
+    pub fn grade(&self, grade: u32) -> Vec<String> {
+        unimplemented!()
+
+        
+    }
+}
